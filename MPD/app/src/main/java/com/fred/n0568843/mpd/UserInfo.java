36822e0c42915881a3ee3d94e365b81eaa4b6001
FredP1 <fred_p1@hotmail.co.uk>
2018-03-06 16:23:41 +0000

    Added Navigation Pane
    Josh can suck my dick

---
+++ MPD/app/src/main/java/com/fred/n0568843/mpd/UserInfo.java
@@ -0,0 +1,19 @@
+package com.fred.n0568843.mpd;
+
+/**
+ * Created by Patricia on 06/03/2018.
+ */
+
+public class UserInfo {
+    String FirstName;
+    String LastName;
+    String Email;
+    String Gender;
+    public UserInfo(String firstName, String lastName, String email, String gender)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Gender = gender;
+    }
+}
